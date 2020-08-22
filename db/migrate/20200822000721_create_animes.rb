16021bdf35b0eba7b82ce850232573b2f22fedd7
Jack Quinoa <jquinoa208@gmail.com>
2020-08-22 01:39:55 +0000

    created tables for migrations

--- db/migrate/20200822000721_create_animes.rb
+++ db/migrate/20200822000721_create_animes.rb
@@ -1,4 +1,9 @@
 class CreateAnimes < ActiveRecord::Migration[5.2]
   def change
+    create_table :animes do |t|
+      t.string :title 
+      t.integer :rating 
+      t.string :comments
+      t.timestamps
   end
 end
