2c733e31c3322bd4723feb06450f4a480dc936b5
Jack Quinoa <jquinoa208@gmail.com>
2020-08-23 21:03:19 +0000

    Added controller actions for views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,28 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
   
   get "/animes" do 
     @animes = Anime.all 
     erb :animes 
+  end
+  
+  get "/sign_up" do 
+    erb :sign_up
+  end
+  
+  get "/sign_in" do 
+    erb :sign_in
+  end
+  
+  get "/details" do 
+    erb :details
+  end
+  
+  get "/add_anime" do 
+    erb :add_anime
   end
   
 end
