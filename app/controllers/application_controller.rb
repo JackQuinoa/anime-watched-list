9d1361ef433aa8c5ae40db74000cc29b896d2255
Jack Quinoa <jquinoa208@gmail.com>
2020-08-23 23:18:45 +0000

    app helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,14 @@ class ApplicationController < Sinatra::Base
   
   helpers do 
     
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  
   end
 
 end
