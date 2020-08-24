ee77369ea37758b980828a12c2c34bd6a6c4ada6
Jack Quinoa <jquinoa208@gmail.com>
2020-08-24 01:06:01 +0000

    fixed user controller post signup redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,31 @@ class UsersController < ApplicationController
   end
   
   post '/signup' do 
-    
+    @user = User.create(params)
+    redirect to '/login'
   end
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   get '/login' do
     erb :'users/login'   
+  end
+  
+  post '/login' do 
+    @user = User.find_by(name:params[:name])
+     if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/animes/animes'
+    else
+      redirect to '/index'
+    end
   end
   
 end
