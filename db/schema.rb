2908a86f446d0d040b7519f5382c46bc5c66c933
Jack Quinoa <jquinoa208@gmail.com>
2020-08-24 02:58:39 +0000

    Successful migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,20 @@
 
 ActiveRecord::Schema.define(version: 2020_08_22_000730) do
 
+  create_table "animes", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.string "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
