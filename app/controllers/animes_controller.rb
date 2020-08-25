2d4827bd0a7535832847d4297f4c97c66d7c15e3
Jack Quinoa <jquinoa208@gmail.com>
2020-08-25 04:42:25 +0000

    updated animes controller and animes views

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,3 +1,12 @@
 class AnimesController < ApplicationController 
   
+  get /'animes/anime_list' do 
+    if logged_in?
+      @list - Anime.all
+      erb :'/animes/anime_list'
+    else 
+      redirect to '/users/login'
+    end
+  end
+  
 end
