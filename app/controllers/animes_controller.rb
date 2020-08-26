4202b306473c9f44d905da99b6daa1019c5a0a40
Jack Quinoa <jquinoa208@gmail.com>
2020-08-26 02:18:15 +0000

    View only presents owned anime to keep lists seperate

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -2,7 +2,7 @@ class AnimesController < ApplicationController
   
   get '/animes/anime_list' do 
     if logged_in?
-      @list = Anime.all
+      @list = Anime.all 
       erb :'/animes/anime_list'
     else 
       redirect to '/users/login'
